836c15e23944aa2f6f30e35df5dc5b5f70549aa0
tapir11 <frytki123@hotmail.com>
2014-05-07 09:18:38 +0200

    Update Source.cpp

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
+
+srand(time(null))
 
 char map[5][13]={
 {'X','X','X','X','X','X','X','X','X','X','X','X','X'},
@@ -15,6 +18,12 @@ const char gracz='@';
 const char ene='!';
 const char kasa='$';
 const char koniec='e';
+
+bool stop=0;
+bool win=0;
+
+int hp=100;
+int pts=0;
 
 struct position
 {
@@ -31,7 +40,46 @@ position enneposold;
 position koniecpos;
 
 
-
+void move(int posy,int posx)
+{
+	char colision = map[posy][posx];
+	
+	if (colision==' ')
+	{
+		graczposold.y=graczpos.y;
+		graczposold.x=graczpos.x;
+		graczpos.y=posy;
+		graczpos.x=posx;
+		
+	}
+	
+	if (colision=='!')
+	{
+		hp-=30;
+		graczpos.y=1;
+		graczpos.x=1;
+		if(hp<=0)
+		stop=1;
+	}
+	
+	
+	//if (collision=='X')
+	
+	if (colision=='e')
+	{
+		win=1
+	}
+	
+	if (collision=='$')
+	{
+		kasa+=10;
+		
+	}
+	
+	
+	
+	
+}
 
 
 
@@ -46,7 +94,39 @@ void main()
 	enepos.x=3;
 	enepos.y=3;
 
-
+while (stop!=1 || win!=1)
+	{
+		
+		
+		if(_kbhit())
+			{
+				switch(getch())
+				
+				case 65: //UP
+				move(graczpos.y-1,graczpos.x)
+				break;
+				
+				
+				case 66:  //DOWN
+				move(graczpos.y+1,graczpos.x)
+				break;
+				
+				
+				case 67: //RIGHT
+				move(graczpos.y,graczpos.x+1)
+				break;
+				
+				
+				case 68: //DOWN
+				move(graczpos.y,graczpos.x-1)
+				
+				
+			
+			
+			}
+		
+		
+	}
 
 
 
